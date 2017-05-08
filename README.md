fb13757ef35b87e632b5fd42dd2e8299af7ee76d
haifanaif <haifa86n@gmail.com>
2017-05-08 23:56:56 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+
+# _happy_cup
---
+++ src/happycup/Simple_ice_cream.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package happycup;
+
+/*
+ * @author FaHad
+ * Programed By Fahad ALfaify
+ * www.fahad.alfaify.net
+ */
+public class Simple_ice_cream {
+
+public String cc(){return "www.fahad.alfaify.net" ;}
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        MainScreen scrMain=new MainScreen();
+        scrMain.setVisible(true);
+    }
+    
+}
